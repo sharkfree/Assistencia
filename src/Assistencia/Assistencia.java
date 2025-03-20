9607625a5ca127eaca07eec793ecae51bdd72d60
shark <vwildt@hotmail.de>
2025-03-20 14:30:08 +0100

    assistencia brasileira

---
+++ src/Assistencia/Assistencia.java
@@ -0,0 +1,12 @@
+package Assistencia;
+
+public class Assistencia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("assistencia brasileira");
+
+	}
+
+}
